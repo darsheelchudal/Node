1d010e08098219ef9d25fc50f7096bd9c5f7bd15
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-14 17:36:05 +0545

    ecommerce modeling

---
+++ models/ecommerce/category.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongooose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Category = mongoose.model("Category", categorySchema);
