c479568f3a7f49d66476e82c806c7117f516f373
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-16 07:59:59 +0545

    Handling URLS using node js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const http = require("http");
 
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New request recieved\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Homepage");
         break;
       case "/about":
-        res.end("I am darsheel");
+        const username = myUrl.query.myname;
+        res.end(`Hi !!,${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`Here are your results for ${search} `);
         break;
       default:
         res.end("404 Not Found");
