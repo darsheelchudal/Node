fd6f94731179f17ba4cefd37509a3cdf2ab4368a
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-16 08:14:22 +0545

    http methods

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New request recieved\n`;
+  const log = `${Date.now()}: ${req.method} : ${
+    req.url
+  } New request recieved\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
